4a50dc52c5b5ef857a471eea6aae4e4d2c8bf8fc
cucudas0127 <cucudas4605@naver.com>
2020-11-11 18:05:02 +0900

    FIX BasicCode/Server.c Recived Part

--- BasicCode/SimpleServer.c
+++ BasicCode/SimpleServer.c
@@ -23,16 +23,18 @@
 
 int flag = 0;
     
-    
+    struct sockaddr_in st_server;
+   
 //-----------------------------------------------------------------------------
 // DESCRIPTS  :Send data to server 
 //-----------------------------------------------------------------------------
 int SendData(int sock_fd, char *tx_buf,int tx_len)
 {
   int itmp; 
-
+  //printf("Send Data : %s\n",tx_buf);
+  
   // Send Packet
-  itmp = send(sock_fd, tx_buf, tx_len, MSG_NOSIGNAL);
+  itmp = send(sock_fd, tx_buf, tx_len);
 
   // Check Tx Status
   if(itmp < 0)  return C_FAIL;   
@@ -55,7 +57,7 @@ void Read_RecivedData(int vSvrFd)
 //-----------------------------------------------------------------------------
 // DESCRIPTS  :Recive data from server 
 //-----------------------------------------------------------------------------
-int RecivedData(struct sockaddr_in st_socket,int sock_fd, char *rx_buf, int rx_buf_size)
+int RecivedData(int sock_fd, char *rx_buf, int rx_buf_size)
 {
   int  	    nData;
   int	    hostAddr_size = sizeof(struct sockaddr_in);
@@ -78,7 +80,7 @@ int RecivedData(struct sockaddr_in st_socket,int sock_fd, char *rx_buf, int rx_b
   if(select(sock_fd+1, &readFds, NULL, NULL, &timeOut) > 0) 
   {
     nData = recvfrom(sock_fd, rx_buf, rx_buf_size, 0,
-                     (struct sockaddr *)&st_socket, &hostAddr_size);
+                     (struct sockaddr *)&st_server, &hostAddr_size);
     return C_SUCCESS;
   }  
   else return C_FAIL;
@@ -95,9 +97,8 @@ int OpenSocket_Server(struct sockaddr_in st_server, int ip_port)
 
     // Create TCP/IP Socket & Init
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);           
-
     if(socket_fd < 0)    return C_FAIL;
-    else                 printf("Socket successfully created..\n");
+
     // Init socket struct
     bzero(&st_server, sizeof(st_server));
 
@@ -108,10 +109,34 @@ int OpenSocket_Server(struct sockaddr_in st_server, int ip_port)
     // Registering a socket in the kernel
     itmp = bind(socket_fd, (struct sockaddr *)&st_server, sizeof(st_server));
     if( itmp != 0)  return C_FAIL;
-    else            printf("Socket successfully binded..\n");
 
     return socket_fd;
 }
+
+//-----------------------------------------------------------------------------
+// DESCRIPTS  : Client Accept
+//-----------------------------------------------------------------------------
+int AcceptClient(int sock_fd)
+{
+    struct sockaddr_in st_client;
+    int itmp = sizeof(st_client);
+    int client_fd;
+
+    //Check client connection request
+    if ((listen(sock_fd, 5)) != 0) 
+        return C_FAIL;
+    
+    
+    //accept client 
+    client_fd = accept(sock_fd, (struct sockaddr *)&st_client, &itmp);
+    if ( client_fd < 0) 
+        return C_FAIL;
+    
+    //if everythin is good
+    return client_fd;
+}
+
+
 
 #define MAX_RX_BUF_SIZE 128
 
@@ -124,47 +149,41 @@ void main(void)
     //================================================
     char rx_buf[128]={0,};
     int sock_fd;
-    struct sockaddr_in st_server, st_client;
-    int itmp,itmp2;
+    int itmp;
+    int client_fd;
 
     // Open Socket
     sock_fd = OpenSocket_Server(st_server, server_port);
     if(sock_fd < 0 ) 
     {
-        printf("Socket Open Error...");
+        printf("server socket Open Error...\n");
         return;
     }
 
-    //Check client connection request
-    if ((listen(sock_fd, 5)) != 0)
+
+    // Client Accept
+    client_fd = AcceptClient(sock_fd);
+    if( client_fd == C_FAIL)
     {
-        printf("server listen failed...\n");
-        return ;
+        printf("server accept Error...\n");
+        return;
     }
 
-    itmp = sizeof(st_client);
-    
-    //accept client 
-    itmp2 = accept(sock_fd, (struct sockaddr *)&st_client, &itmp);
-    if (itmp2 < 0)
+    for(int i = 5; i> 0; i--)
     {
-        printf("server acccept failed...\n");
-        return ;
-    }
-    
-    // Read Data
-    while (1)
-    {
-        if(RecivedData(&st_server, sock_fd,rx_buf,MAX_RX_BUF_SIZE) ==C_SUCCESS)
-            break;
-    }
-    printf("Recived Data : %s\n",rx_buf);
+        // Read Data
+        //Read_RecivedData(client_fd);
+        while(RecivedData(client_fd, rx_buf, MAX_RX_BUF_SIZE)==C_FAIL);
+        printf("[%d] Recived Data : %s\n",i,rx_buf);
 
-    // Send Data
-    SendData(sock_fd,send_message,strlen(send_message));
+        // Send Data
+        SendData(client_fd,send_message,strlen(send_message));
+    }
 
     //close socket
     close(sock_fd);
 
     return ;
 }
+
+
